46aef86e20790ee7a001ac9315f2d5dac11379db
hanairokoromo <tneri.0mc5.rin2do2@gmail.com>
2023-05-29 03:02:01 +0000

    Update

--- sneakers/js/style.js
+++ sneakers/js/style.js
@@ -19,6 +19,48 @@ $(function(){
    $('#navi a').on('click', function(){
        $('#header').removeClass('open');
    });
+   
+   
+   //PICK UPスライダー
+   $('.slick-area').slick({
+       arrows: false,
+       centerMode: true,
+       centerPadding: '100px',
+       slidesToShow: 3,
+       responsive:[
+           {
+               breakpoint: 768,
+               settings:{
+                   centerPadding: '50px',
+                   slidesToShow: 1
+               }
+           }
+           ]
+   });
+   
+   
+   //スムーススクロール
+   $('a[href^="#"]').click(function(){
+       let href = $(this).attr("href");
+       let target = $(href == "#" || href == "" ? 'html' : href);
+       let position = target.offset().top;
+       $("html, body").animate({scrollTop:position}, 600, "swing");
+       return false;
+   });
+    
+    
+    //スクロール時の画像のフェード表示
+    $(window).scroll(function(){
+        $('fadein').each(function(){
+            let scroll = $(window).scrollTop();
+            let target = $(this).offset().top;
+            let windowHeigth = $(window).height();
+            if(scroll > target - windowHeigth + 200){
+                $(this).css('opacity', '1');
+                $(this).css('transform', 'translateY(0)');
+            }
+        });
+    });
     
     
 });
